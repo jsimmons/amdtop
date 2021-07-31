b3b103f3323cc80fe7bc86d56b72c98688c8b3a3
Joshua Simmons <simmons44@gmail.com>
2021-07-31 18:54:43 +0200

    Fix GPU enumeration

--- src/main.rs
+++ src/main.rs
@@ -37,115 +37,117 @@ fn log(x: u64, base: u64) -> u64 {
     }
 }
 
+struct FormatBytes {
+    bytes: u64,
+}
+impl FormatBytes {
+    fn new(bytes: u64) -> Self {
+        Self { bytes }
+    }
+}
+impl Display for FormatBytes {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        const DIVISOR: u64 = 1024;
+        const SUFFIXES: &[&'static str] = &["", "KiB", "MiB", "GiB"];
+
+        if self.bytes == 0 {
+            return self.bytes.fmt(f);
+        }
+
+        let divisions = std::cmp::min(log(self.bytes, DIVISOR), SUFFIXES.len() as u64);
+        let result = self.bytes as f64 / DIVISOR.pow(divisions as u32) as f64;
+        format!("{:.2} {}", result, SUFFIXES[divisions as usize]).fmt(f)
+    }
+}
+
+#[derive(Default, Copy, Clone)]
+struct MemInfo {
+    pid: i32,
+    gtt_bytes: u64,
+    vram_bytes: u64,
+    unknown_bytes: u64,
+}
+
 fn main() -> Result<(), io::Error> {
-    let gpu_index = 0;
-    let gem_info_path = format!("/sys/kernel/debug/dri/{}/amdgpu_gem_info", gpu_index);
+    for entry in glob::glob("/sys/kernel/debug/dri/*/amdgpu_gem_info").unwrap() {
+        if let Ok(gem_info_path) = entry {
+            let mut mem_infos = HashMap::<i32, MemInfo>::new();
+            let mut cur_pid = -1;
 
-    #[derive(Default, Copy, Clone)]
-    struct MemInfo {
-        pid: i32,
-        gtt_bytes: u64,
-        vram_bytes: u64,
-        unknown_bytes: u64,
-    }
-
-    let mut mem_infos = HashMap::<i32, MemInfo>::new();
-    let mut cur_pid = -1;
-
-    let mut process_line = |line: &str| -> Option<()> {
-        let mut segments = line.split_whitespace();
-        match segments.next()? {
-            "pid" => {
-                let pid = segments.next()?;
-                if let Ok(pid) = pid.parse() {
-                    cur_pid = pid;
+            let mut process_line = |line: &str| -> Option<()> {
+                let mut segments = line.split_whitespace();
+                match segments.next()? {
+                    "pid" => {
+                        let pid = segments.next()?;
+                        if let Ok(pid) = pid.parse() {
+                            cur_pid = pid;
+                        }
+                    }
+                    _ => {
+                        let bytes = str::parse::<u64>(segments.next()?).ok()?;
+                        let _skip = segments.next()?;
+                        let memory_type = segments.next()?;
+                        let mem_info = mem_infos.entry(cur_pid).or_default();
+                        match memory_type {
+                            "VRAM" => mem_info.vram_bytes += bytes,
+                            "GTT" => mem_info.gtt_bytes += bytes,
+                            _ => mem_info.unknown_bytes += bytes,
+                        }
+                    }
                 }
+
+                Some(())
+            };
+
+            for line in read_lines(gem_info_path)? {
+                process_line(&line?);
             }
-            _ => {
-                let bytes = str::parse::<u64>(segments.next()?).ok()?;
-                let _skip = segments.next()?;
-                let memory_type = segments.next()?;
-                let mem_info = mem_infos.entry(cur_pid).or_default();
-                match memory_type {
-                    "VRAM" => mem_info.vram_bytes += bytes,
-                    "GTT" => mem_info.gtt_bytes += bytes,
-                    _ => mem_info.unknown_bytes += bytes,
+
+            let mut mem_infos_sorted = mem_infos
+                .iter()
+                .map(|(pid, mem_info)| MemInfo {
+                    pid: *pid,
+                    ..*mem_info
+                })
+                .collect::<Vec<_>>();
+
+            mem_infos_sorted.sort_by_key(|mem_info| {
+                std::cmp::Reverse(mem_info.vram_bytes + mem_info.gtt_bytes)
+            });
+
+            println!(
+                "{0: <10} | {1: <20} | {2: <60} | {3: >15} | {4: >15} | {5: >15}",
+                "PID", "PROCESS", "PATH", "TOTAL", "VRAM", "GTT"
+            );
+
+            println!("{:-^1$}", "", 150);
+
+            for mem_info in mem_infos_sorted {
+                if mem_info.pid == -1 {
+                    continue;
                 }
+
+                let path = std::fs::read_link(format!("/proc/{}/exe", mem_info.pid))
+                    .map(|path| path.to_string_lossy().to_owned().to_string());
+                let name = std::fs::read_to_string(format!("/proc/{}/comm", mem_info.pid));
+
+                println!(
+                    "{0: <10} | {1: <20} | {2: <60} | {3: >15} | {4: >15} | {5: >15}",
+                    mem_info.pid,
+                    name.as_ref()
+                        .map(String::as_str)
+                        .map(str::trim)
+                        .unwrap_or("unknown"),
+                    path.as_ref()
+                        .map(String::as_str)
+                        .map(str::trim)
+                        .unwrap_or("unknown"),
+                    FormatBytes::new(mem_info.vram_bytes + mem_info.gtt_bytes),
+                    FormatBytes::new(mem_info.vram_bytes),
+                    FormatBytes::new(mem_info.gtt_bytes),
+                );
             }
         }
-
-        Some(())
-    };
-
-    for line in read_lines(gem_info_path)? {
-        process_line(&line?);
-    }
-
-    let mut mem_infos_sorted = mem_infos
-        .iter()
-        .map(|(pid, mem_info)| MemInfo {
-            pid: *pid,
-            ..*mem_info
-        })
-        .collect::<Vec<_>>();
-
-    mem_infos_sorted
-        .sort_by_key(|mem_info| std::cmp::Reverse(mem_info.vram_bytes + mem_info.gtt_bytes));
-
-    struct FormatBytes {
-        bytes: u64,
-    }
-    impl FormatBytes {
-        fn new(bytes: u64) -> Self {
-            Self { bytes }
-        }
-    }
-    impl Display for FormatBytes {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            const DIVISOR: u64 = 1024;
-            const SUFFIXES: &[&'static str] = &["", "KiB", "MiB", "GiB"];
-
-            if self.bytes == 0 {
-                return self.bytes.fmt(f);
-            }
-
-            let divisions = std::cmp::min(log(self.bytes, DIVISOR), SUFFIXES.len() as u64);
-            let result = self.bytes as f64 / DIVISOR.pow(divisions as u32) as f64;
-            format!("{:.2} {}", result, SUFFIXES[divisions as usize]).fmt(f)
-        }
-    }
-
-    println!(
-        "{0: <10} | {1: <20} | {2: <40} | {3: >15} | {4: >15} | {5: >15}",
-        "PID", "PROCESS", "PATH", "TOTAL", "VRAM", "GTT"
-    );
-
-    println!("{:-^1$}", "", 130);
-
-    for mem_info in mem_infos_sorted {
-        if mem_info.pid == 0 {
-            continue;
-        }
-
-        let path = std::fs::read_link(format!("/proc/{}/exe", mem_info.pid))
-            .map(|path| path.to_string_lossy().to_owned().to_string());
-        let name = std::fs::read_to_string(format!("/proc/{}/comm", mem_info.pid));
-
-        println!(
-            "{0: <10} | {1: <20} | {2: <40} | {3: >15} | {4: >15} | {5: >15}",
-            mem_info.pid,
-            name.as_ref()
-                .map(String::as_str)
-                .map(str::trim)
-                .unwrap_or("unknown"),
-            path.as_ref()
-                .map(String::as_str)
-                .map(str::trim)
-                .unwrap_or("unknown"),
-            FormatBytes::new(mem_info.vram_bytes + mem_info.gtt_bytes),
-            FormatBytes::new(mem_info.vram_bytes),
-            FormatBytes::new(mem_info.gtt_bytes),
-        );
     }
 
     Ok(())
